0b35eb8040064e76ef4813b8bb36425fb4bb5807
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2026-06-09 23:16:57 +0530

    refactor: update user module and service, add user schema, and create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    fname!: string
+
+    @IsString()
+    @IsNotEmpty()
+    lname!: string
+
+    @IsString()
+    @IsNotEmpty()
+    email!: string
+
+    @IsString()
+    @IsNotEmpty()
+    password!: string
+}
