f0953c8dfad66ca21628f3396d156c5b10fc7e43
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2026-06-25 12:28:46 +0530

    feat: set global prefix for API routes to 'api'

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api')
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
 
